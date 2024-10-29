ef99e64cc1d70867142dc125c6fa6ae2ae9a0db6
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-10-29 17:21:44 -0500

    feat: added a skeleton for the auth button

---
+++ src/components/skeletons/AuthButtonSkeleton.tsx
@@ -0,0 +1,7 @@
+export default function AuthButtonSkeleton() {
+    return (
+        <div className="flex gap-4 items-center">
+            <div className="w-32 h-8 bg-gray-200 animate-pulse rounded-lg" />
+        </div>
+    );
+}
