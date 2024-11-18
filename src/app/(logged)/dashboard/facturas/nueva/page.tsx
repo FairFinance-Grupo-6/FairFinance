b20a3a13d80b85963b6cea5952ddd0ffeeb9607b
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-11-18 11:20:57 -0500

    feat(facturas): guardar nueva factura en la base de datos
    Se modificó la función handleSaveFactura en el archivo page.tsx para guardar una nueva factura en la base de datos. Se creó un objeto invoice_to_save con los datos necesarios de la factura y se insertó en la tabla "documents" en la base de datos. Esto permite almacenar la información de la factura para su posterior uso.

--- src/app/(logged)/dashboard/facturas/nueva/page.tsx
+++ src/app/(logged)/dashboard/facturas/nueva/page.tsx
@@ -6,7 +6,35 @@ import { supabase } from "@/utils/supabase/client";
 export default function NuevaFactura() {
   const handleSaveFactura = async (facturaData: any) => {
     try {
-      await supabase.from("facturas").insert([facturaData]);
+      const invoice_to_save = {
+        idFactura: facturaData.id,
+        fechaEmision: facturaData.fechaEmision,
+        fechaVencimiento: facturaData.fechaVencimiento,
+        plazoDescuento: parseInt(facturaData.plazoDescuento),
+        importeNominal: facturaData.importeNominal,
+        moneda: facturaData.moneda,
+        tipoTasa: facturaData.tipoTasa,
+        tiempoTasa: facturaData.tiempoTasa,
+        capitalizacion: facturaData.capitalizacion,
+        valorTasa: parseInt(facturaData.valorTasa),
+        costosAdicionales: facturaData.costosAdicionales.reduce((acc: number, costo: any) => {
+          return acc + parseFloat(costo.monto);
+        }, 0),
+        costosMora: facturaData.costosMora.reduce((acc: number, costo: any) => {
+          return acc + parseFloat(costo.monto);
+        }, 0),
+        portes: parseInt(facturaData.portes),
+        retencion: parseInt(facturaData.retencion),
+        tipoTasaMora: facturaData.tipoTasaMora,
+        tiempoTasaMora: facturaData.tiempoTasaMora,
+        capitalizacionMora: facturaData.capitalizacionMora,
+        valorTasaMora: facturaData.valorTasaMora,
+        diasMora: parseInt(facturaData.diasMora) || 0,
+        conMora: facturaData.conMora,
+        tcea: facturaData.tcea,
+      };
+
+      await supabase.from("documents").insert([invoice_to_save]);
     } catch (err) {
       console.error("Error inesperado:", err);
     }
