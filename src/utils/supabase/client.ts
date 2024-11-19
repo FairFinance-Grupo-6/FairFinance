17c25103ad2f4a603c35f4775e8ff3778c1c163a
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-11-19 11:29:47 -0500

    fix(supabase): actualizar cliente para usar createBrowserClient
    Se actualizó el cliente de Supabase para utilizar la función createBrowserClient en lugar de createClient. Esto permite que el cliente funcione correctamente en entornos de servidor y de navegador.

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,14 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl =
+	process.env.NEXT_PUBLIC_SUPABASE_URL ||
+	(() => {
+		throw new Error("NEXT_PUBLIC_SUPABASE_URL is not defined");
+	})();
+const supabaseKey =
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+	(() => {
+		throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined");
+	})();
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
