2324ad7f36c96bc0973e8756f75bd6212dd3b8b4
TMedalith <u20211f955@upc.edu.pe>
2024-11-20 15:03:17 -0500

    feat: Refactor layout for improved navigation and responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,51 +37,49 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link
-                      href={"/"}
-                      className="hover:text-[#5756BB] transition duration-200 ease-in-out"
-                    >
-                      Fair Finance
-                    </Link>
-                    <Link
-                      href={"/dashboard"}
-                      className="hover:text-[#5756BB] transition duration-200 ease-in-out"
-                    >
-                      Dashboard
-                    </Link>
-                    <Link
-                      href={"/dashboard/facturas"}
-                      className="hover:text-[#5756BB] transition duration-200 ease-in-out"
-                    >
-                      Facturas
-                    </Link>
-                    <Link
-                      href={"/dashboard/cartera"}
-                      className="hover:text-[#5756BB] transition duration-200 ease-in-out"
-                    >
-                      Cartera
-                    </Link>
-                  </div>
-                  <div className="flex gap-5 items-center">
-                    <Suspense fallback={<AuthButtonSkeleton />}>
-                      {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-                    </Suspense>
-                    <ThemeSwitcher />
-                  </div>
+          <main className="min-h-screen h-screen flex flex-col items-center">
+            <nav className="fixed top-0 left-0 w-full flex justify-center border-b border-b-foreground/10 h-16 bg-white dark:bg-background z-50">
+              <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                <div className="flex gap-5 items-center font-semibold">
+                  <Link
+                    href={"/"}
+                    className="hover:text-[#5756BB] transition duration-200 ease-in-out"
+                  >
+                    Fair Finance
+                  </Link>
+                  <Link
+                    href={"/dashboard"}
+                    className="hover:text-[#5756BB] transition duration-200 ease-in-out"
+                  >
+                    Dashboard
+                  </Link>
+                  <Link
+                    href={"/dashboard/facturas"}
+                    className="hover:text-[#5756BB] transition duration-200 ease-in-out"
+                  >
+                    Facturas
+                  </Link>
+                  <Link
+                    href={"/dashboard/cartera"}
+                    className="hover:text-[#5756BB] transition duration-200 ease-in-out"
+                  >
+                    Cartera
+                  </Link>
                 </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
+                <div className="flex gap-5 items-center">
+                  <Suspense fallback={<AuthButtonSkeleton />}>
+                    {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+                  </Suspense>
+                  <ThemeSwitcher />
+                </div>
               </div>
+            </nav>
+            <div className="flex flex-1 w-full min-h-full  max-w-5xl mt-16 p-5">
+              {children}
             </div>
           </main>
         </ThemeProvider>
       </body>
-    </html >
+    </html>
   );
 }
