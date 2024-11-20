28cbebe80a1336e16dbd4cb510b139142a1aea09
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-11-20 14:52:10 -0500

    feat(middleware): add server-side middleware for authentication and redirection

---
+++ src/middleware.ts
@@ -0,0 +1,63 @@
+import { createServerClient } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
+
+export const middleware = async (request: NextRequest) => {
+	try {
+		let response = NextResponse.next({
+			request: {
+				headers: request.headers,
+			},
+		});
+
+		const supabase = createServerClient(
+			process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+			{
+				cookies: {
+					getAll() {
+						return request.cookies.getAll();
+					},
+					setAll(cookiesToSet) {
+						for (const { name, value } of cookiesToSet) {
+							request.cookies.set(name, value);
+						}
+						response = NextResponse.next({
+							request,
+						});
+						for (const { name, value, options } of cookiesToSet) {
+							response.cookies.set(name, value, options);
+						}
+					},
+				},
+			},
+		);
+
+		const {
+			data: { user },
+		} = await supabase.auth.getUser();
+
+		const needLoggedIn = ["/dashboard", "/facturas"];
+
+		if (needLoggedIn.includes(request.nextUrl.pathname) && !user) {
+			return NextResponse.redirect(new URL("/sign-in", request.url));
+		}
+
+		if (request.nextUrl.pathname === "/sign-in" && user) {
+			return NextResponse.redirect(new URL("/dashboard", request.url));
+		}
+		if (request.nextUrl.pathname === "/sign-up" && user) {
+			return NextResponse.redirect(new URL("/dashboard", request.url));
+		}
+		if (request.nextUrl.pathname === "/" && user) {
+			return NextResponse.redirect(new URL("/dashboard", request.url));
+		}
+
+		return response;
+	} catch (e) {
+		return NextResponse.next({
+			request: {
+				headers: request.headers,
+			},
+		});
+	}
+};
